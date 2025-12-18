fe2ea13e9388549ea1ad5f5e1a62aecb45e1accb
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-12-18 12:41:18 +0530

    Program.cs

--- secondproj/Program.cs
+++ secondproj/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The Current Time is: " + DateTime.Now);
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name: ");
+        string? name = Console.ReadLine();
+        Console.WriteLine("Hello " + name + "!");
+    }
+}
--- secondproj/obj/Release/net10.0/secondproj.AssemblyInfo.cs
+++ secondproj/obj/Release/net10.0/secondproj.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("secondproj")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1c2ccbdfbbac43aabe8e705ce952724966ae9d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("secondproj")]
