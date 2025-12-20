ee4cd09f02486bf38127f49d632361d509829758
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2025-12-20 10:21:55 +0530

    Classes

---
+++ classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Program
+{
+
+    class Student
+    {        
+        public int id;
+        public string name;
+        public int age;
+        public char gender;
+            
+    }
+
+    public static void Main(string[] args)
+    {
+        Student s1 = new Student();
+
+        s1.id = 1;
+        s1.name = "Rohan Roy";
+        s1.age = 19;
+        s1.gender = 'M';
+
+
+        Console.WriteLine(s1.id);
+        Console.WriteLine(s1.name);
+        Console.WriteLine(s1.age);
+        Console.WriteLine(s1.gender);
+    }
+
+
+
+}
---
+++ practice/easy15/Admission.cs
@@ -0,0 +1,75 @@
+using System;
+
+class Admission {
+
+
+    // Function that checks if Candidate is Eligible
+    public static bool IsEligible(int mathmarks, int phymarks, int chemmarks)
+    {
+        int total = mathmarks + phymarks + chemmarks;
+
+        if(mathmarks>=65 && phymarks>=55 && chemmarks>=50 && ((total>=180) || ((mathmarks+phymarks)>=140)))
+        {
+          return true;
+        }
+
+        return false;
+
+    }
+
+    // Function for taking user input
+    static string GetInput(string message)
+    {
+        Console.Write(message);
+        string inp = Console.ReadLine();
+        return inp;
+    }
+
+
+    // Function to convert string input into integer
+    static int ProcessInput(string input)
+    {
+
+        // converting string input to integer
+        try
+        {
+            if(int.TryParse(input, out int mark1))
+            {
+                return mark1;
+            }
+
+            return 0;
+        }
+
+        catch
+        {
+            return 0;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+
+        // Taking Math marks input
+        string inp1 = GetInput("Enter Marks in Math:");
+        int mathmarks = ProcessInput(inp1);
+
+        // Taking Physics marks input
+        string inp2 = GetInput("Enter Marks in Physics:");
+        int phymarks = ProcessInput(inp2);
+
+        // Taking Chemistry marks input
+        string inp3 = GetInput("Enter Marks in Chemistry: ");
+        int chemmarks = ProcessInput(inp3);
+
+
+        // Printing result if student is eligible or not
+        Console.WriteLine("The Student is Eligible: " + IsEligible(mathmarks, phymarks, chemmarks));
+
+
+    }
+
+
+
+
+}
