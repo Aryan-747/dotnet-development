7f38ba1927a84b6ac39ad1c55353b9a69fcc1a0b
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2026-02-24 14:59:33 +0530

    WebD

---
+++ Day47WebDev/BusinessLayer/BusinessLayer/Class1.cs
@@ -0,0 +1,18 @@
+﻿using DAL;
+
+namespace BusinessLayer
+{
+    public class BLReverse
+    {
+        public string ReverseString()
+        {
+            DLString obj = new DLString();
+
+            string x = obj.GetAllNames();
+
+            x = new string(x.Reverse().ToArray());
+
+            return x;
+        }
+    }
+}
---
+++ Day47WebDev/Frontend/Frontend/Program.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer;
+
+class Program
+{
+    public  static void PrintData()
+    {
+        BLReverse obj = new BLReverse();
+
+        string x = obj.ReverseString();
+
+        Console.WriteLine(x);
+    }
+
+    public static void Main(string[] args)
+    {
+        PrintData();
+    }
+}
