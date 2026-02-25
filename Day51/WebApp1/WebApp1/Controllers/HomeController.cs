b6c253219921880abdcd9ab4058e5b6f9214bca9
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2026-02-25 16:41:20 +0530

    WebD

---
+++ Day51/WebApp1/WebApp1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string data = "India,Spain,UK,Russia,China,Ukraine,Italy,US,Mexico,Argentina";
+
+            string[] itemList = data.Split(',');
+
+            return View(itemList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
