61afcf6ba457c2e2f7d1e557312ce36a735886a8
Aryan Salaria <118845675+Aryan-747@users.noreply.github.com>
2026-03-08 18:31:18 +0530

    Completed Sunday Assignment

---
+++ Day59Assignment/LibraryManagement/LibraryManagement/Program.cs
@@ -0,0 +1,33 @@
+using LibraryManagement.Repositories;
+using LibraryManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IBookRepository, SqlBookRepository>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Book}/{action=List}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
